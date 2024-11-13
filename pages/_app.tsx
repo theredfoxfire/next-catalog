18c46388425c8a36092b157592f876adcbe83769
ventalicdev@gmail.com <ventalicdev@gmail.com>
2024-11-13 16:55:19 +0700

    fix(next-catalog): fake commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import dynamic from 'next/dynamic';
 import { type AppProps } from 'next/app';
 
-// lazzy loading component
 const Navigation = dynamic(() => import('@/src/components/navigation/Navigation'), {
     ssr: false, 
   });
